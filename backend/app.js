7e7bc3e73d45d369dcd8fc3f78f5fd7ec89cdc22
James Kim <junyeongkim1@gmail.com>
2024-05-28 14:13:45 -0400

    Making changes

--- backend/app.js
+++ backend/app.js
@@ -53,9 +53,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-	.connect(
-		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hkhlfkd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0?authSource=admin`
-	)
+	.connect(process.env.MONGO_URI)
 	.then(() => {
 		app.listen(port);
 	})
