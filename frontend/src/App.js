30e9aef5c986616da206b2a4f0fd1171228fd1f6
James Kim <junyeongkim1@gmail.com>
2024-05-29 13:10:51 -0400

    Removed lazy loading to prevent loading for a long time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
-import React, { Suspense } from "react";
+import React from "react";
 import {
 	BrowserRouter as Router,
 	Route,
 	Redirect,
 	Switch,
 } from "react-router-dom";
-import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
 
-// import Users from './user/pages/Users';
-// import NewPlace from "./places/pages/NewPlace";
-// import UserPlaces from "./places/pages/UserPlaces";
-// import UpdatePlace from "./places/pages/UpdatePlace";
-// import Auth from "./user/pages/Auth";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
 
 // Lazy loading (Code splitting)
-const Users = React.lazy(() => import("./user/pages/Users"));
-const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
-const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
-const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
-const Auth = React.lazy(() => import("./user/pages/Auth"));
+// const Users = React.lazy(() => import("./user/pages/Users"));
+// const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+// const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+// const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+// const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 const App = () => {
 	const { token, login, logout, userId } = useAuth();
@@ -75,17 +74,7 @@ const App = () => {
 		>
 			<Router>
 				<MainNavigation />
-				<main>
-					<Suspense
-						fallback={
-							<div className="center">
-								<LoadingSpinner />
-							</div>
-						}
-					>
-						{routes}
-					</Suspense>
-				</main>
+				<main>{routes}</main>
 			</Router>
 		</AuthContext.Provider>
 	);
